302722c973419f98d35394273be352dd8ad3f692
Nadia Humbert-Labeaumaz <np.humbert@gmail.com>
2016-05-07 15:10:31 +0200

    feature(*): Implement move forward

--- src/main/java/com/nphumbert/mars/Instructions.kt
+++ src/main/java/com/nphumbert/mars/Instructions.kt
@@ -1,5 +1,7 @@
 package com.nphumbert.mars
 
+import java.util.*
+
 interface Instruction {
     fun apply(position: Position): Position
 }
@@ -11,8 +13,18 @@ class RotateLeft : Instruction {
 }
 
 class MoveForward : Instruction {
+
+    val moves = HashMap<Orientation, (Position) -> Position>()
+
+    constructor() {
+        moves.put(Orientation.NORTH, { Position(it.x, it.y.plus(1), it.orientation) })
+        moves.put(Orientation.EAST, { Position(it.x.plus(1), it.y, it.orientation) })
+        moves.put(Orientation.SOUTH, { Position(it.x, it.y.minus(1), it.orientation) })
+        moves.put(Orientation.WEST, { Position(it.x.minus(1), it.y, it.orientation) })
+    }
+
     override fun apply(position: Position): Position {
-        throw UnsupportedOperationException()
+        return moves[position.orientation]!!.invoke(position)
     }
 }
 
--- src/test/java/com/nphumbert/mars/InstructionsTest.kt
+++ src/test/java/com/nphumbert/mars/InstructionsTest.kt
@@ -51,4 +51,28 @@ class RotateRightTest {
 
 }
 
+class MoveForwardTest {
+
+    @Test
+    fun should_increment_y_when_facing_north_and_move_forward() {
+        assertThat(MoveForward().apply(Position(1, 2, Orientation.NORTH))).isEqualTo(Position(1, 3, Orientation.NORTH))
+    }
+
+    @Test
+    fun should_increment_x_when_facing_east_and_move_forward() {
+        assertThat(MoveForward().apply(Position(1, 2, Orientation.EAST))).isEqualTo(Position(2, 2, Orientation.EAST))
+    }
+
+    @Test
+    fun should_decrement_y_when_facing_south_and_move_forward() {
+        assertThat(MoveForward().apply(Position(1, 2, Orientation.SOUTH))).isEqualTo(Position(1, 1, Orientation.SOUTH))
+    }
+
+    @Test
+    fun should_decrement_x_when_facing_west_and_move_forward() {
+        assertThat(MoveForward().apply(Position(1, 2, Orientation.WEST))).isEqualTo(Position(0, 2, Orientation.WEST))
+    }
+
+}
+
 
